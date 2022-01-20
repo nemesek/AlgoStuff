73a026d4bfaf6be50a0164951b20ec01a1ddcd1d
nemesek <dnemesek@gmail.com>
2022-01-20 12:42:27 -0600

    solution for two sums

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -8,7 +8,7 @@
 # 0 <= Node.val <= 9
 # It is guaranteed that the list represents a number that does not have leading zeros.
 
-from typing import Optional
+from typing import List, Optional
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -29,11 +29,45 @@ class Solution:
             power += 1
 
         return number
+
+    def __convert_int_to_ListNode(self, n: int, node: Optional[ListNode]) -> Optional[ListNode]:
+
+        val = n % 10
+        n = n//10
+        node.val = val
+        if n > 0:
+            node.next = self.__convert_int_to_ListNode(n, ListNode())
+        return node
     
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         number1 = self.__calculate_number(l1)
         number2 = self.__calculate_number(l2)
-        print(number1 + number2)
-        return None
+        sum = number1 + number2
+        answer = self.__convert_int_to_ListNode(sum, ListNode())
+        return answer
 
-
+if __name__ == "__main__":
+    sol = Solution()
+    node1 = ListNode(3, None)
+    node2 = ListNode(4, node1)
+    node3 = ListNode(2, node2)
+    node4 = ListNode(4, None)
+    node5 = ListNode(6, node4)
+    node6 = ListNode(5, node5)
+    answer1 = sol.addTwoNumbers(node3, node6)
+    node1 = ListNode(0, None)
+    node4 = ListNode(0, None)
+    answer2 = sol.addTwoNumbers(node1, node4)
+    node1 = ListNode(9, None)
+    node2 = ListNode(9, node1)
+    node3 = ListNode(9, node2)
+    node4 = ListNode(9, node3)
+    node5 = ListNode(9, node4)
+    node6 = ListNode(9, node5)
+    node7 = ListNode(9, node6)
+    node8 = ListNode(9, None)
+    node9 = ListNode(9, node8)
+    node10 = ListNode(9, node9)
+    node11 = ListNode(9, node10)
+    answer3 = sol.addTwoNumbers(node7, node11)
+    print('done')
